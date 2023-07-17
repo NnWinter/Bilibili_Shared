839d2a36963aff91b3491fcccbbd86b607cf122e
NnWinter <515621078@163.com>
2023-07-17 09:33:59 +0800

    技能切换器获取位置

---
+++ Programs/NET7/20230716_DiabloMpAltDisplay/技能切换器/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+using 技能切换器;
+
+Bitmap bmp = (Bitmap)Image.FromFile("Sample.png");
+
+var g = Graphics.FromImage(bmp);
+
+var sizeH = 3;
+
+var bar = Skills.GetBarLocs(Screen.PrimaryScreen);
+for (int i = 0; i < bar.Length; i++)
+{
+    var loc = bar[i];
+    g.DrawRectangle(new Pen(Color.Red), loc.X - sizeH, loc.Y - sizeH, sizeH * 2, sizeH * 2);
+}
+
+g.Flush();
+
+bmp.Save("Debug.png");
---
+++ Programs/NET7/20230716_DiabloMpAltDisplay/技能切换器/Skills.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 技能切换器
+{
+    internal class Skills
+    {
+        private static readonly Vector2 SKILL_START = new Vector2(0.4375f, 0.3451f);
+        private static readonly Vector2 SKILL_PADDING = new Vector2(0.0329f, 0.0670f);
+
+        private static readonly Vector2 BAR_START = new Vector2(0.4183f, 0.9306f);
+        private static readonly float BAR_PADDING = 0.0328f;
+
+        public static Point[,] GetSkillLocs(Screen screen)
+        {
+            var skills = new Point[6, 6];
+            var screenSize = GetScreenSize(screen);
+
+            for (int y = 0; y < 6; y++)
+            {
+                for (int x = 0; x < 6; x++)
+                {
+                    var pos = (SKILL_START + new Vector2(x, y) * SKILL_PADDING) * screenSize;
+                    skills[x, y] = new Point((int)pos.X, (int)pos.Y);
+                }
+            }
+            return skills;
+        }
+        public static Point[] GetBarLocs(Screen screen)
+        {
+            var screenSize = GetScreenSize(screen);
+
+            var bars = new Point[6];
+            for (int i = 0; i < 6; i++)
+            {
+                var pos = (BAR_START + new Vector2(i, 0) * BAR_PADDING) * screenSize;
+                bars[i] = new Point((int)pos.X, (int)pos.Y);
+            }
+            return bars;
+        }
+        private static Vector2 GetScreenSize(Screen screen)
+        {
+            return new Vector2(screen.Bounds.Width, screen.Bounds.Height);
+        }
+    }
+}
